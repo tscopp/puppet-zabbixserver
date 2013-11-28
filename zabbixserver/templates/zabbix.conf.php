1d0d54f42bab1a31c45bf73d2423a2bef1cf17e8
Timothy Scoppetta <tms@eecs.berkeley.edu>
2013-11-28 01:45:55 -0800

    initial commit of zabbixserver puppet module

---
+++ zabbixserver/templates/zabbix.conf.php
@@ -0,0 +1,17 @@
+<?php
+// Zabbix GUI configuration file
+global $DB;
+
+$DB['TYPE']     = 'MYSQL';
+$DB['SERVER']   = 'localhost';
+$DB['PORT']     = '0';
+$DB['DATABASE'] = 'zabbix';
+$DB['USER']     = 'zabbix';
+$DB['PASSWORD'] = 'isdg_1123098210283917';
+
+$ZBX_SERVER      = 'localhost';
+$ZBX_SERVER_PORT = '10051';
+$ZBX_SERVER_NAME = 'imon-1.eecs.berkeley.edu';
+
+$IMAGE_FORMAT_DEFAULT = IMAGE_FORMAT_PNG;
+?>
